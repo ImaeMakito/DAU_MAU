efc8d1db4379e4b517f7bd20230073916a8c6544
imae_makito <imae_makito@example.com>
2018-03-16 18:36:53 +0900

    second commit

--- DAU_not_regi.sql
+++ DAU_not_regi.sql
@@ -1,1 +1,46 @@
-DAU_not_regi
+/*
+既存のactive_hourly.sqlに、os情報を追加
+registerログを除外
+ */
+with whole as (
+  select distinct
+    user_id,
+    platform as os,
+    timestamp_trunc(timestamp, hour) as logged_at
+  from
+    `warm-calculus-736.liverpool.action_*`
+  where
+    _table_suffix between  '20180310' and '20180315' and
+    type != 'register' 
+    -- registerログは、１ユーザーに対して複数発行される可能性があるため、ログインUUのカウントから除外する
+  union all
+  select distinct
+    user_id,
+    platform as os,
+    timestamp_trunc(timestamp, hour) as logged_at
+  from
+    `warm-calculus-736.liverpool.audio_*`
+  where
+    _table_suffix between  '20180310' and '20180315'
+    
+  union all
+  select distinct
+    user_id,
+    platform as os,
+    timestamp_trunc(timestamp, hour) as logged_at
+  from
+    `warm-calculus-736.liverpool.search_*`
+  where
+   _table_suffix between  '20180310' and '20180315' 
+)
+select distinct
+  count(distinct user_id) as count_user,
+  os,
+  date(logged_at,'Asia/Tokyo') as date_jst
+ -- timestamp_add(timestamp_trunc(timestamp,hour), interval 9 hour) as timestamp_jst
+from
+  whole
+where
+logged_at >= '2018-03-10' 
+group by 
+ os,date_jst
